dc4c0fcd97a49f6517b95fec442519419665c4f0
Jasminekk <54400905+Jasminekk@users.noreply.github.com>
2019-08-22 21:42:00 +0630

    Update check_word.java

--- check_word.java
+++ check_word.java
@@ -2,25 +2,9 @@ public class check_word{
 
     public static void main(String[] args) {
 
-        String[] word={"l","e","v","e","l"};
-        int count=0,j=4;
-
-
-        for (int i = 0; i <2 ; i++) {
-
-            if (word[i]==word[j]){
-                count++;
-                j--;
-            }
-        } //End Loop
-
-
-        if (count==2){
-            System.out.println("ITS A PALINDROME");
-        }
-        else {
-            System.out.println("ITS NOT A PALINDROME");
-        }
+        
+            System.out.println("Testing");
+      
 
     }
 }
